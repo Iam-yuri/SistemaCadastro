01f3d539015c52cc3b34ed374528374b0dbab8dc
Iam-yuri <reis17.yuriprado@gmail.com>
2025-06-24 03:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaCadastro/Controllers/CadastroController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCadastro.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult verificarCPF(string cpf)
+        {
+            if (cpf == "12345678900")
+            {
+                return RedirectToAction("BoasVindas", new { nome = "Yuri" });
+            }
+            else
+            {
+                return RedirectToAction("PerguntarCadastro", new { cpf });
+            }
+        }
+
+        public IActionResult BoasVindas(string nome)
+        {
+            ViewBag.Nome = nome;
+            return View();
+        }
+
+        public IActionResult PerguntarCadastro(string cpf)
+        {
+            ViewBag.CPF = cpf;
+            return View();
+        }
+
+        public IActionResult Formulario(string cpf)
+        {
+            ViewBag.CPF = cpf;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SalvarCadastro(string cpf, string nome, string email, string empresa, string cargo, string estado, string uf, bool expositor, string cnpj)
+        {
+            return RedirectToAction("Confirmacao");
+        }
+
+        public IActionResult confirmacao()
+        {
+            return View();
+        }
+    }
+}
